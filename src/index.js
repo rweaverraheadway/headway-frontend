62ee12ae2a40dc75294503946437c8e88b05f202
rweaverraheadway <98102890+rweaverraheadway@users.noreply.github.com>
2022-01-22 12:39:02 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { render } from "react-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://20rwt.sse.codesandbox.io/",
+  cache: new InMemoryCache()
+});
+
+function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+    </div>
+  );
+}
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
